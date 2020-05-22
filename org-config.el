8c6cff7c9481c63ee29cf22a280d22f9c55173b0
Robert Irelan <rirelan@gmail.com>
2020-05-22 11:37:24 -0700

    Work around https://github.com/hlissner/doom-emacs/issues/3185

--- org-config.el
+++ org-config.el
@@ -1584,6 +1584,14 @@ Follows the same rules as `org-agenda-files'"
 (setq! org-agenda-sticky t)
 
 (setq! org-list-allow-alphabetical t)
+
+;; See https://github.com/hlissner/doom-emacs/issues/3185
+(defadvice! no-errors/+org-inline-image-data-fn (_protocol link _description)
+  :override #'+org-inline-image-data-fn
+  "Interpret LINK as base64-encoded image data. Ignore all errors."
+  (ignore-errors
+    (base64-decode-string link)))
+
 ;;; Local Variables:
 ;;; outline-regexp: ";;;\\*+\\|\\`"
 ;;; End:
