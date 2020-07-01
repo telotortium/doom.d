b296aae9b04456b06a324e8e5b95372a68061a0a
Robert Irelan <rirelan@gmail.com>
2020-07-01 10:33:13 -0700

    Set custom file location

--- config.el
+++ config.el
@@ -3,6 +3,8 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+(setq! custom-file (expand-file-name "custom.el" doom-private-dir))
+(load custom-file)
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
