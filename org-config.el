a154ffb1827258d7bf6d904f4de3383a58b7304e
Robert Irelan <rirelan@gmail.com>
2020-10-19 09:27:04 -0700

    Use doom `map!` macro to define org-fc keys

--- org-config.el
+++ org-config.el
@@ -987,18 +987,18 @@ don't support wrapping."
      (reviews . (:filter (not (tag "inbox")))))))
 (after! (evil org-fc)
   (add-to-list 'evil-emacs-state-modes 'org-fc-dashboard-mode)
-  (evil-define-minor-mode-key '(normal insert emacs) 'org-fc-review-flip-mode
-    (kbd "RET") 'org-fc-review-flip
-    (kbd "n") 'org-fc-review-flip
-    (kbd "s") 'org-fc-review-suspend-card
-    (kbd "q") 'org-fc-review-quit)
-  (evil-define-minor-mode-key '(normal insert emacs) 'org-fc-review-rate-mode
-    (kbd "a") 'org-fc-review-rate-again
-    (kbd "h") 'org-fc-review-rate-hard
-    (kbd "g") 'org-fc-review-rate-good
-    (kbd "e") 'org-fc-review-rate-easy
-    (kbd "s") 'org-fc-review-suspend-card
-    (kbd "q") 'org-fc-review-quit))
+  (map! :map org-fc-review-flip-mode-map
+        :me "RET" #'org-fc-review-flip
+        :me "n" #'org-fc-review-flip
+        :me "s" #'org-fc-review-suspend-card
+        :me "q" #'org-fc-review-quit)
+  (map! :map org-fc-review-rate-mode-map
+        :me "a" #'org-fc-review-rate-again
+        :me "h" #'org-fc-review-rate-hard
+        :me "g" #'org-fc-review-rate-good
+        :me "e" #'org-fc-review-rate-easy
+        :me "s" #'org-fc-review-suspend-card
+        :me "q" #'org-fc-review-quit))
 (defun org-fc-review-inbox ()
   "Run ‘org-fc’ review on the ‘inbox’ context (see ‘org-fc-custom-contexts')."
   (interactive)
