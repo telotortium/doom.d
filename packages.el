d3ba1c6fa9866648ed7455df5cb75b9b1b81ed47
Robert Irelan <rirelan@gmail.com>
2020-11-10 20:52:20 -0800

    Fix org-mode build recipe to set org-version correctly
    Since
    https://github.com/hlissner/doom-emacs/commit/911118227cba1dd66e066bdbf78224193b3a8de9#diff-9be23fd924f084574d83d4d968639de518ab8ae4ac17d3d0c8b0459292a59751,
    `org-version` is set in the build recipe, so we need to copy it to our
    local org-mode install. Fixes https://github.com/hlissner/doom-emacs/issues/4248.

--- packages.el
+++ packages.el
@@ -71,7 +71,19 @@
 (package! org-mode
   :recipe (:host github :repo "emacs-straight/org-mode"
            :fork (:repo "git@github.com:telotortium/org-mode"
-                  :host nil :branch "feature-drawer-text-prop")))
+                  :host nil :branch "feature-drawer-text-prop")
+           :files ("*.el" "lisp/*.el" "contrib/lisp/*.el")
+           ;; HACK A necessary hack because org requires a compilation step
+           ;;      after being cloned, and during that compilation a
+           ;;      org-version.el is generated with these two functions, which
+           ;;      return the output of a 'git describe ...'  call in the repo's
+           ;;      root. Of course, this command won't work in a sparse clone,
+           ;;      and more than that, initiating these compilation step is a
+           ;;      hassle, so...
+           :build (with-temp-file (expand-file-name "org-version.el" (straight--repos-dir "org-mode"))
+                    (insert "(fset 'org-release (lambda () \"9.4\"))\n"
+                            "(fset 'org-git-version #'ignore)\n"
+                            "(provide 'org-version)\n"))))
 (package! org-clock-csv)
 (package! org-pomodoro
   :recipe (:host github :repo "marcinkoziej/org-pomodoro"
