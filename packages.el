d7710d0a43e4d04e4ca79f7f1cdbf08ab6b29b16
Robert Irelan <rmirelan@google.com>
2021-07-14 18:15:31 -0700

    Fix parinfer-mode to not error on `evil-define-key`

--- packages.el
+++ packages.el
@@ -112,5 +112,7 @@
 (package! hl-line+)
 (package! evil-visual-mark-mode)
 
+(package! parinfer-mode :recipe (:host github :repo "Wagk/parinfer-mode" :branch "patch-1"))
+
 (package! explain-pause-mode
   :recipe (:host github :repo "lastquestion/explain-pause-mode"))
