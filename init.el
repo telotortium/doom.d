8c20fede5fbcf63c8fa27aafaa09628c27c90bd7
Robert Irelan <rirelan@gmail.com>
2021-09-07 12:37:00 -0700

    Use ivy-prescient in hopes of faster Ivy speed

--- init.el
+++ init.el
@@ -29,7 +29,7 @@
        company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ivy               ; a search engine for love and life
+       (ivy +prescient)    ; a search engine for love and life
 
        :ui (modeline +light)
        ;;deft              ; notational velocity for Emacs
