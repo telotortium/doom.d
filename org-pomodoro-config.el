bba52dbbb5463461da5e30ad22c235534aeb25aa
Robert Irelan <rirelan@gmail.com>
2022-05-11 12:27:52 -0700

    my-org-pomodoro-info-today: allow specifying date besides today

--- org-pomodoro-config.el
+++ org-pomodoro-config.el
@@ -346,11 +346,20 @@ will work as designed."
 (defvar my-org-pomodoro-time-today-var 0
   "Amount of time spent in pomodoro today, in seconds.")
 
-(defun my-org-pomodoro-info-today ()
+(defun my-org-pomodoro-info-today (&optional date)
   "Show count of pomodoros and time spent within today."
-  (interactive)
+  (interactive
+   (pcase current-prefix-arg
+     ('nil nil)
+     ('(4) (list (org-read-date nil t nil "Pomodoro info for day: ")))
+     (_
+      (user-error "unknown prefix arg: %S" current-prefix-arg))))
   (message "Updating info...")
-  (let ((process-environment (copy-sequence process-environment)))
+  (let ((process-environment (copy-sequence process-environment))
+        (today-start
+         (append `(0 0 ,(or org-extend-today-until 0))
+                 (nthcdr 3 (decode-time
+                            (or date (org-current-effective-time)))))))
     (when my-org-pomodoro-browser
       (setenv "BROWSER" my-org-pomodoro-browser))
     (with-current-buffer (generate-new-buffer "*org_pomodoro_calendar_log_sum.py*")
@@ -361,16 +370,20 @@ will work as designed."
              "org_pomodoro_calendar_log_sum.py"
              (current-buffer)
              (expand-file-name "org_pomodoro_calendar_log_sum.py" doom-private-dir)
-             (let* ((today-start
-                     (append `(0 0 ,(or org-extend-today-until 0))
-                             (nthcdr 3 (decode-time (org-current-effective-time))))))
-               (append
-                (list
-                 "--calendar_id" my-org-pomodoro-log-gcal-calendar-id
-                 "--state" ":pomodoro"
-                 "--start_timestamp" (format-time-string "%FT%T%z"
-                                                         (encode-time today-start))
-                 "--end_timestamp" (format-time-string "%FT%T%z" (current-time))))))))
+             (append
+              (list
+               "--calendar_id" my-org-pomodoro-log-gcal-calendar-id
+               "--state" ":pomodoro"
+               "--start_timestamp" (format-time-string "%FT%T%z"
+                                                       (encode-time today-start))
+               "--end_timestamp"
+               (format-time-string
+                "%FT%T%z"
+                (let* ((ct (current-time))
+                       ;; Add 1 day to today-start
+                       (today-end
+                        (time-add (encode-time today-start) (* 24 60 60))))
+                  (if (time-less-p today-end ct) today-end ct))))))))
         ;; This process timeout code from
         ;; https://emacs.stackexchange.com/a/10295/17182. Not using CLI
         ;; ‘timeout’ program because it doesn’t exist on macOS.
@@ -385,14 +398,16 @@ will work as designed."
                       (string-trim (buffer-substring-no-properties
                                     (point-min) (point-max))))
                      (result-list (s-split "," out)))
-                (setq my-org-pomodoro-count-today-var
-                      (string-to-number (nth 0 result-list))
-                      my-org-pomodoro-time-today-var
-                      (string-to-number (nth 1 result-list)))
-                (message "Org Pomodoro - Count: %2d, Time: %s"
-                         my-org-pomodoro-count-today-var
-                         (org-timer-secs-to-hms
-                          (round my-org-pomodoro-time-today-var)))))))))))
+                (when (= 2 (length result-list))
+                  (setq my-org-pomodoro-count-today-var
+                        (string-to-number (nth 0 result-list))
+                        my-org-pomodoro-time-today-var
+                        (string-to-number (nth 1 result-list)))
+                  (message "Org Pomodoro %s - Count: %2d, Time: %s"
+                           (format-time-string "%Y-%m-%d" (encode-time today-start))
+                           my-org-pomodoro-count-today-var
+                           (org-timer-secs-to-hms
+                            (round my-org-pomodoro-time-today-var))))))))))))
 (defun my-org-pomodoro-finished-info-today ()
   "Run ‘my-org-pomodoro-info-today’ when Pomodoro finishes."
   (display-warning
