14b846cf6ee3f0c93f86f11a9842c4d7863e01ce
Robert Irelan <rirelan@gmail.com>
2024-08-08 19:21:43 -0700

    Fix formatting

--- init.el
+++ init.el
@@ -51,7 +51,7 @@
              (shell-command-to-string
               (format "cd %s && git rev-parse HEAD"
                       (shell-quote-argument
-                        (expand-file-name doom-emacs-dir)))))))
+                       (expand-file-name doom-emacs-dir)))))))
         (setq actual-commit result))
     (error
      (error "Verifying commit of %1$s failed: %2$S
@@ -125,7 +125,7 @@ cd %1$s && git reset --hard %3$s
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
-       ;(parinfer +rust)    ; turn lisp into python, sort of
+       ;;(parinfer +rust)    ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
        snippets            ; my elves. They type so I don't have to
        ;;word-wrap         ; soft wrapping with language-aware indent
